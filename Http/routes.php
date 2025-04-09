ea487cceca8e1617b79b0708d55b7f501bcbb921
Magneticdud <Magneticdud@users.noreply.github.com>
2025-04-09 23:51:52 +0200

    very alpha

--- Http/routes.php
+++ Http/routes.php
@@ -3,4 +3,11 @@
 Route::group(['middleware' => 'web', 'prefix' => \Helper::getSubdirectory(), 'namespace' => 'Modules\FetchNow\Http\Controllers'], function()
 {
     Route::get('/', 'FetchNowController@index');
+    Route::get('/fetch-emails', function() {
+        Artisan::call('freescout:fetch-emails', [
+            '--days' => 3,
+            '--unseen' => 0
+        ]);
+        return redirect()->back()->with('success', 'Emails fetched successfully!');
+    })->middleware('auth');
 });
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FetchNow
 A module for freescout that adds a "Fetch now" button for all users
+
+## How to use
+
+As of now there is no public facing button, just go to yourfreescoutURL.com/fetch-emails to fetch the latest 3 days, including read emails
